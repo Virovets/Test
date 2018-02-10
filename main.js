4adf1f14c53ea38ec07b50e7c0af701e15824696
Anton <32961106+Virovets@users.noreply.github.com>
2018-02-10 10:12:10 +0200

    done

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
-window.onscroll = function() {
-    var scrolled = window.pageYOffset;
-    if (scrolled > 100) {
-        document.querySelector('header').classList.add("sticky");
+window.addEventListener('scroll', stickyHeader);
+var header = document.querySelector('header');
+var headerTop = header.getBoundingClientRect().top + window.pageYOffset;
+var flag = true;
+
+function stickyHeader() {
+
+    if (window.pageYOffset < headerTop) {
+        header.classList.remove('sticky');
+        flag = true;
+    } else if (window.pageYOffset > headerTop && flag) {
+        header.classList.add('sticky');
+        header.style.opacity = 0;
+        fade(header, 1000, 50);
+        flag = false;
     } else {
-        document.querySelector('header').classList.remove("sticky");
+        clearInterval(fade);
     }
 }
+
+function fade(elem, t, f) {
+    var fps = f || 50;
+    var time = t || 500;
+    var steps = time / fps;
+    var op = 0;
+    var d0 = 1 / steps;
+    var timer = setInterval(function() {
+        op += d0;
+        header.style.opacity = op;
+        steps--;
+        if (steps == 0) {
+            clearInterval(timer);
+        }
+    }, (1000 / fps));
+
+};
